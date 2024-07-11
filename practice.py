8377cc104e996f86df5ed70b02cb62b210962f94
khj6775 <khj6775@hotmail.com>
2024-07-11 21:52:56 +0900

    0711

--- practice.py
+++ practice.py
@@ -13,14 +13,16 @@ y = np.array([1,2,3,5,4,6])
 #2. 모델구현
 model = Sequential()
 model.add(Dense(3, input_dim =1))
-model.add(Dense(4))
-model.add(Dense(5))
-model.add(Dense(3))
-model.add(Dense(1, input_dim =3))
+model.add(Dense(6))
+model.add(Dense(12))
+model.add(Dense(25))
+model.add(Dense(12))
+model.add(Dense(6))
+model.add(Dense(1, input_dim =6))
 
 
 #3. 컴파일, 훈련
-epochs = 100
+epochs = 1000
 model.compile(loss='mse', optimizer='adam')
 model.fit(x, y, epochs = epochs, batch_size=2)
 
@@ -35,3 +37,9 @@ print("7의 예측값 : ", result)
 # epochs =  2000
 # 로스 :  0.3284194767475128
 # 7의 예측값 :  [[6.6936216]]  
+
+
+import numpy as np
+
+x1 = np.array([1,2,3])
+print("x1 : ", x1.shape)
