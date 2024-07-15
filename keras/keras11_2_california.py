09152bf88168468112dc2e6f4149ce21b47e3efa
khj6775 <khj6775@hotmail.com>
2024-07-15 21:56:34 +0900

    20240715

--- keras/keras11_2_california.py
+++ keras/keras11_2_california.py
@@ -18,8 +18,8 @@ print(x.shape, y.shape)     #(20640, 8) (20640,)
 
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7,
-                                                    random_state=9000)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.9,
+                                                    random_state=3)
 print('x_train :', x_train)
 print('x_test :', x_test)
 print('y_train :', y_train)
@@ -36,7 +36,7 @@ model.add(Dense(1))
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=50, batch_size=16)
+model.fit(x_train, y_train, epochs=500, batch_size=32)
 
 #4. 평가, 예측
 loss = model.evaluate(x_test, y_test)
@@ -46,3 +46,11 @@ y_predict = model.predict(x_test)
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
 print("r2스코어 : ", r2)
+
+# (x, y, train_size=0.75, random_state=5000) epochs=100, batch_size=20
+# 로스 :  0.5952420234680176
+# r2스코어 :  0.5487004137289405
+
+# (x, y, train_size=0.9, random_state=3) epochs=1000, batch_size=32
+# 로스 :  0.5491809248924255
+# r2스코어 :  0.5924301575839137
